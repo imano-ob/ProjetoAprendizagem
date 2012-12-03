35335cd19670bfc84b36d202a2ad83bdf6dab678
Rewasvat <fernando_aluani@hotmail.com>
2012-12-03 19:02:33 -0200

    Adicionei script ridiculo pra rodar N classificadores

---
+++ classifierTest.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys, os
+
+classifiers = ["BAYES", "KNN", "DTREE"]
+descriptors = ["FAST", "STAR", "SURF"]
+extractors = ["SIFT", "SURF", "ORB"]
+
+def Execute(argList):
+    possibilities = [(d, e) for d in descriptors for e in extractors]
+    classifierType = argList[0]
+    for descriptorType, extractorType in possibilities:
+        cmdLine = "./LandmarkDetector %s %s %s sets/training/ sets/test/" % (classifierType, descriptorType, extractorType)
+        print "RUNNING ", cmdLine
+        os.system(cmdLine)
+        print "======================================================================================="
+    #possibilities = [(c, d, e) for c in classifiers for d in descriptors for e in extractors]
+    #
+    #for classifierType, descriptorType, extractorType in possibilities:
+    #    cmdLine = "./LandmarkDetector %s %s %s sets/training/ sets/test/" % (classifierType, descriptorType, extractorType)
+    #    print "RUNNING ", cmdLine
+    #    os.system(cmdLine)
+    #    print "======================================================================================="
+
+if __name__ == '__main__':
+    Execute(sys.argv[1:])
