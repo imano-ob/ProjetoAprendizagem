a4d2f9e4b7245b4e90a30341d2163124b689be57
Rewasvat <fernando_aluani@hotmail.com>
2012-12-05 12:06:21 -0200

    Arrumei o output do programa pra ficar mais facilmente parseavel; Tirei junk code e lixo comentado; Mudei numClusters pra 128;

--- src/baseclassifier.cc
+++ src/baseclassifier.cc
@@ -17,15 +17,11 @@ typedef std::vector< cv::KeyPoint > KPVector;
 
 
 BaseClassifier::BaseClassifier(const std::string& detectorName, const std::string& extractorName) {
-    //detector_ = Ptr<cv::FeatureDetector>(new cv::SurfFeatureDetector(400)); //minHessian = 400
     detector_ = cv::FeatureDetector::create(detectorName);
-    //extractor_ = Ptr<cv::DescriptorExtractor>(new cv::SurfDescriptorExtractor());
     extractor_ = Ptr<cv::DescriptorExtractor>( cv::DescriptorExtractor::create(extractorName) );
     matcher_ = Ptr<cv::DescriptorMatcher>(new cv::FlannBasedMatcher());
-    trainer_ = Ptr<cv::BOWTrainer>(new cv::BOWKMeansTrainer(50)); //numClusters = 1000
+    trainer_ = Ptr<cv::BOWTrainer>(new cv::BOWKMeansTrainer(128)); //numClusters = 1000
     bowide_ = Ptr<cv::BOWImgDescriptorExtractor>(new cv::BOWImgDescriptorExtractor(extractor_, matcher_));
-    //classifier_ = Ptr<CvRTrees>( new CvRTrees() );
-    //classifier_ = Ptr<CvNormalBayesClassifier>( new CvNormalBayesClassifier() );
 }
 BaseClassifier::~BaseClassifier() {
 }
@@ -43,6 +39,7 @@ void BaseClassifier::RunTraining(ImageSet& set) {
     KPVector keypoints;
     
     cout << "Getting descriptors" << endl;
+    time_t mark = time(NULL);
     ImgInfoVector images = set.GetAllImages();
     ImgInfoVector::iterator it;
     for (it = images.begin(); it != images.end(); ++it) {
@@ -55,11 +52,13 @@ void BaseClassifier::RunTraining(ImageSet& set) {
     }
     //printMatType(training_descriptors, "TRAINING DESCRIPTORS");
     trainer_->add(training_descriptors);
+    cout << "Descriptors acquired (in " << (time(0)-mark) << " seconds)" << endl;
+    
     cout << "Going to clusterize" << endl;
-    time_t mark = time(NULL);
+    mark = time(NULL);
     vocabulary_ = trainer_->cluster();
     bowide_->setVocabulary(vocabulary_);
-    printf("Vocabulary has been calculated (in %ld seconds)\n", (time(NULL)-mark));
+    cout << "Vocabulary has been calculated (in " << (time(0)-mark) << " seconds)" << endl;
     //printMatType(vocabulary_, "VOCABULARY");
     
     /*Step2: generate image histograms using the vocabulary
@@ -89,12 +88,7 @@ void BaseClassifier::RunTraining(ImageSet& set) {
     //printMatType(floatSamples, "FLOATSAMPLES");
     //printMatType(labels, "LABELS");
     
-    //CvMat porra = floatSamples;
-    //printf("FloatSamples is MAT = %d\n", CV_IS_MAT(&porra) );
-    //printf("FloatSamples type check = %d = %d\n", CV_MAT_TYPE(porra.type), CV_32FC1 );
     mark = time(NULL);
-    //classifier_->train(floatSamples, CV_ROW_SAMPLE, labels);  //was using this for CvRTrees classifier
-    //classifier_->train(floatSamples, labels); //was using this for CvNormalBayesClassifier classifier
     doTrain(floatSamples, labels);
     cout << "Classifier has been trained (in " << (time(0)-mark) << " seconds)" << endl;
 }
@@ -114,20 +108,16 @@ void BaseClassifier::RunTests(ImageSet& set) {
         bowide_->compute(img, keypoints, hist);
         it->set_histogram(hist);
         
-        float label = doPredict(hist); //classifier_->predict(hist);
+        float label = doPredict(hist);
         float expected = it->label();
         
+        cout << "TEST " << label << " " << it->classname() << " " << expected;
         if (label == expected) {
             hits++;
-            cout << "{HUZZAH}[" << it->classname() << "] " << it->name() << ": predicted as " << label << " (expecting " << expected << ")" << endl;
+            cout << " OK";
         }
-        else {
-            cout << "{------}[" << it->classname() << "] " << it->name() << ": predicted as " << label << " (expecting " << expected << ")" << endl;
-        }
+        cout << endl;
     }
-    
-    cout << "Total number of correct results: ";
-    cout << hits << "/" << images.size() << endl;
 }
     
 
