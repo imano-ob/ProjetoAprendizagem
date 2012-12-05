d3999915ab2e893605e254cce685a95387931dae
Renan T. Carneiro <renanteruoc@gmail.com>
2012-12-05 13:01:03 -0200

    Parser guarda melhor combinação de classifier agora baseado no resultado do ainda inexistente parser

--- logParser.py
+++ logParser.py
@@ -11,9 +11,15 @@ def parse():
 
 def Execute(argList):
     possibilities = [(c, d, e) for c in classifiers for d in descriptors for e in extractors]
+    bestRate = 0
     for classifierType, descriptorType, extractorType in possibilities:
         logfileName = classifierType + '_' + descriptorType + '_' + extractorType + '_log'
         successRate = parse(logFileName)
+        if successRate > bestRate:
+            bestRate = successRate
+            bestDescriptor = descriptorType
+            bestExtractor = extractorType
+            bestClassifier = classifierType
 
 if __name__ -- '__main__':
     Execute(sys.argv[1:])
